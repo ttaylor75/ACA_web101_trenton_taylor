5d7e61ec53f32c7f8df1817b013fe51c14105d5e
ttaylor75 <ttaylor75@outlook.com>
2020-11-07 14:57:55 -0600

    interactive resume js animation

---
+++ resume-interactive.js
@@ -0,0 +1,13 @@
+const name = document.getElementById("name");
+
+name.addEventListener('click', () => name.style.opacity = '0');
+
+
+const phone = document.getElementById("phone");
+
+phone.addEventListener('click', () => phone.style.opacity = '1');
+
+
+const profilePic = document.getElementById("profilePic");
+
+profilePic.addEventListener('mouseover', () => profilePic.style.opacity = '0');
